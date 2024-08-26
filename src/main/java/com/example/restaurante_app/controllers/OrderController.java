c5ebf73460f9ec918a05daaafea714c5d2e0d51d
Leo Silva Souza <13321466+leovano@users.noreply.github.com>
2024-08-26 14:40:28 -0300

    set missing orders endpoint

--- src/main/java/com/example/restaurante_app/controllers/OrderController.java
+++ src/main/java/com/example/restaurante_app/controllers/OrderController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/orders")
 public class OrderController {
     private final OrderService orderService;
     private final OrderSpecificationBuilder orderSpecificationBuilder;
